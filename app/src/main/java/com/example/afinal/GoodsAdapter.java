5db299598c6be08901ad75418f8fd3e4a50c879f
429040506 <429040506@qq.com>
2021-12-24 05:17:31 +0800

    增加了积分商城listview的修改

--- app/src/main/java/com/example/afinal/GoodsAdapter.java
+++ app/src/main/java/com/example/afinal/GoodsAdapter.java
@@ -9,43 +9,36 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.afinal.R;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MyAdapter extends BaseAdapter {
-
-    //新闻列表集合
-    private List<NewsData> datas=new ArrayList<NewsData>();
+public class GoodsAdapter extends BaseAdapter {
+    private List<GoodsData> datas=new ArrayList<GoodsData>();
     private Context context;
     private LayoutInflater layoutInflater;
 
-    public MyAdapter(Context context,List<NewsData> datas){
+    public GoodsAdapter(Context context,List<GoodsData> datas){
         this.context=context;
         this.datas=datas;
         this.layoutInflater=LayoutInflater.from(context);
     }
 
-    //返回列表长度
     @Override
     public int getCount() {
         return datas.size();
     }
 
-    //通过列表位置获取集合中的对象
     @Override
-    public NewsData getItem(int i) {
+    public GoodsData getItem(int i) {
         return datas.get(i);
     }
 
-    //获取集合的item的位置
     @Override
     public long getItemId(int i) {
         return i;
@@ -56,36 +49,31 @@ public class MyAdapter extends BaseAdapter {
         if(view==null){
             //找到布局文件
             view=layoutInflater.inflate(R.layout.list_item, null);
-            view.setTag(new ViewHolder(view));
+            view.setTag(new GoodsAdapter.ViewHolder(view));
         }
-        initViews(getItem(i),(ViewHolder) view.getTag());
+        initViews(getItem(i),(GoodsAdapter.ViewHolder) view.getTag());
         return view;
     }
 
-    //初始化数据
-    private void initViews(NewsData data, ViewHolder holder){
-        //设置tag防止错位
-        holder.ivImg.setTag(data.getNewsImgUrl());
-        //设置新闻标题
-        holder.tvTitle.setText(data.getNewsTitle());
-        //设置新闻时间
-        holder.tvDate.setText(data.getNewsDate());
-        //通过集合中的图片地址来获得图片并且设置到view上
-        getImage(this.context,data.getNewsImgUrl(),holder.ivImg);
+    private void initViews(GoodsData data, GoodsAdapter.ViewHolder holder){
+        holder.goods_img.setTag(data.getGoodsImgUrl());
+        holder.goods_name.setText(data.getGoodsName());
+        holder.goods_price.setText(data.getGoodsPrice());
+        getImage(this.context,data.getGoodsImgUrl(),holder.goods_img);
     }
 
     protected class ViewHolder {
-        private ImageView ivImg;
-        private TextView tvTitle;
-        private TextView tvDate;
+        private ImageView goods_img;
+        private TextView goods_name;
+        private TextView goods_price;
 
         public ViewHolder(View view) {
-            ivImg = (ImageView) view.findViewById(R.id.iv_img);
-            tvTitle = (TextView) view.findViewById(R.id.tv_title);
-            tvDate = (TextView) view.findViewById(R.id.tv_date);
+            goods_img = (ImageView) view.findViewById(R.id.iv_img);
+            goods_name = (TextView) view.findViewById(R.id.tv_title);
+            goods_price = (TextView) view.findViewById(R.id.tv_date);
         }
     }
-    //检测图片的Tag值 ,如果根请求的地址相同 才做图片的网络请求
+
     public void getImage(Context context, String imgUrl, final ImageView imageView) {
         if (imageView.getTag().toString().equals(imgUrl)) {
             RequestQueue mQueue = Volley.newRequestQueue(context);
@@ -104,4 +92,3 @@ public class MyAdapter extends BaseAdapter {
         }
     }
 }
-
